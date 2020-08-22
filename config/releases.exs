809aeb9d5a21de2e17d6bf5c05f98a8c1ecff577
Alvaro <alvaro.villen@gmail.com>
2020-08-22 19:12:57 +0200

    Make all ports configurables

--- config/releases.exs
+++ config/releases.exs
@@ -2,8 +2,8 @@ import Config
 
 config :hyta, HytaWeb.Endpoint,
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
-  http: [port: String.to_integer(System.fetch_env!("PORT"))],
+  http: [port: String.to_integer(System.fetch_env!("HTTP_PORT"))],
   url: [
     host: System.get_env("HOST"),
-    port: 443
+    port: String.to_integer(System.fetch_env!("URL_PORT"))
   ]
